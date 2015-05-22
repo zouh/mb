fd4428bd967498184b5d0ef18e5daf0a091d4528
zouh <zouh74@gmail.com>
2015-05-22 18:08:46 +0800

    Add Weixin Support

--- app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
+++ app/decorators/controllers/weixin_rails_middleware/weixin_controller_decorator.rb
@@ -119,9 +119,9 @@ WeixinRailsMiddleware::WeixinController.class_eval do
       # user = 1.to_s
       # a = @keyword
       url ＝ 'https://weituike.herokuapp.com/users/#{user}/qrcode'
-      redirect_to url
-      reply_text_message("你点击了: #{@keyword}, #{request.original_url}, #{request.host}, #{request.url}, #{request.original_fullpath} ")
 
+      reply_text_message("你点击了: #{@keyword}, #{request.host}, #{url} ")
+      redirect_to url
       #redirect_to '/users/1/qrcode'
     end
 
