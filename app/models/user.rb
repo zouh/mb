7a57f882703ba44241e6a7cabf97dab6bfffbb44
zouh <zouh74@gmail.com>
2015-05-22 17:56:08 +0800

    Add Weixin Support

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ActiveRecord::Base
 
   enum role: [ :visitor, :member, :vip, :partner, :angel, :master, :admin ]
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, length: { minimum: 6 }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  # validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  # has_secure_password
+  # validates :password, length: { minimum: 6 }
 
 
   #default_scope { order('organization_id').order('name') }
